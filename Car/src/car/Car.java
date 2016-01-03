c52d6bac85ad31d6a0202b9fee213fd87bab60ac
Premdeep Amudala <preamdhep.usapp@gmail.com>
2016-01-03 00:08:48 -0800

    Added custom constructor with methods

--- Car/src/car/Car.java
+++ Car/src/car/Car.java
@@ -9,30 +9,76 @@ public class Car {
 
     int maxSpeed = 100;
     int minSpeed = 0;
-    boolean isCarOk = true;
+    int passengers = 1;
+    
+    int maxFuel = 16;
+    double currentFuel = 8;
+    int mpg = 15;
+    
+    double distanceTravelled;
+    double maxDistance;
+    
+    int weight = 4096;
+    //boolean isCarOk = true;
     char Status = 'G';
+    
+    
+    // Constructor for custom initialization in contrast with the default values
+    public Car(int customMaxSpeed, int customWeight, int customMpg ){
+        maxSpeed = customMaxSpeed;
+        weight = customWeight;
+        mpg = customMpg;
+    }
+    
+    public Car(){// default constructor
+    }
+    
+    public double maxmilage(){
+        return mpg * maxFuel;
+    }
+    
+    public void getIN(){
+        passengers++;
+    }
+    
+    public void getOut(){
+        
+        passengers--;
+    }
+        
+    
+    
     
    public void printVariables(){
        System.out.println("The max speed is: " + maxSpeed);
-       System.out.println("The min speed is: " + minSpeed);
-       System.out.println(isCarOk);
-       System.out.println(Status);
+       System.out.println("The min speed is: " + minSpeed);       
+       System.out.println("Car milage :"+mpg);
+       System.out.println("weigt of car :"+ weight);
+       System.out.println("No of passengers :" + passengers);
    }
    
-   public void wreck(){
-       isCarOk = false;
-       Status = 'P';
+  
+   
+   public void upgradespeed(){
+       minSpeed = maxSpeed;
+       maxSpeed += 1;
    }
     public static void main(String[] args) {
         Car Honda = new Car(); // created an instance of the class car with object Honda
         System.out.println("Honda car brand new Condition ");
-        Honda.printVariables();   
-        Car familyCar = Honda;  // Referencing "Shared car" which means family car is the Honda car
-        familyCar.wreck();
-        System.out.println("Honda car after family car use");
-        Honda.printVariables();
-        System.out.println("Family car afer wreck");
-        familyCar.printVariables();
-    }
+        Honda.printVariables(); 
+        
+        Car BMW = new Car(500,2000,'A');
+        BMW.upgradespeed();       
+        System.out.println("BMW updated values");
+        BMW.printVariables();
+        BMW.getIN();
+        BMW.getIN();
+        System.out.println("passengers :"+BMW.passengers);
+        BMW.getOut();
+        System.out.println("max fuel is :"+BMW.maxmilage());
+        
+        
+            }
     
 }

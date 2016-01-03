c011b14d5adae75bb1da9788f42c2cadaca54ac2
Premdeep Amudala <preamdhep.usapp@gmail.com>
2016-01-02 16:05:50 -0800

    First Java Program

---
+++ HelloPrem/src/helloprem/HelloPrem.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloprem;
+
+/**
+ *
+ * @author Prem
+ */
+public class HelloPrem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Day 0 of code");
+    }
+    
+}

cb0dbef020ad951ded841caf08132cbba8cd6111
Premdeep Amudala <preamdhep.usapp@gmail.com>
2016-01-03 15:57:58 -0800

    Added if else statements to the Car Class

--- Car/src/car/Car.java
+++ Car/src/car/Car.java
@@ -9,7 +9,10 @@ public class Car {
 
     int maxSpeed = 100;
     int minSpeed = 0;
-    int passengers = 1;
+    
+    int noOfPassengers = 1;
+    int maxPassengers = 5;
+    
     
     int maxFuel = 16;
     double currentFuel = 8;
@@ -19,7 +22,7 @@ public class Car {
     double maxDistance;
     
     int weight = 4096;
-    //boolean isCarOk = true;
+    boolean isCarOn = false;
     char Status = 'G';
     
     
@@ -30,20 +33,43 @@ public class Car {
         mpg = customMpg;
     }
     
-    public Car(){// default constructor
+    // default constructor
+    public Car(){
     }
     
+    public void isCarOn(){
+        isCarOn = true;
+    }
     public double maxmilage(){
         return mpg * maxFuel;
     }
     
     public void getIN(){
-        passengers++;
+        if(noOfPassengers < maxPassengers){
+        noOfPassengers++;
+        System.out.println("No of passengers : "+noOfPassengers);
+        }
+        else{
+            System.out.println("The Car is Full"+ noOfPassengers + "="+ maxPassengers);
+        }
     }
     
     public void getOut(){
+        if (noOfPassengers > 1){
+        noOfPassengers--;
+        System.out.println("No of passengers : "+noOfPassengers); 
+        }
+        else{
+            System.out.println("No passengers in the Car");
+         
+        if(noOfPassengers == 1){
+            System.out.println("Only Driver is in the Car");
+        }
+            
+        }
+            
         
-        passengers--;
+        
     }
         
     
@@ -54,7 +80,7 @@ public class Car {
        System.out.println("The min speed is: " + minSpeed);       
        System.out.println("Car milage :"+mpg);
        System.out.println("weigt of car :"+ weight);
-       System.out.println("No of passengers :" + passengers);
+       System.out.println("No of passengers :" + noOfPassengers);
    }
    
   
@@ -64,20 +90,19 @@ public class Car {
        maxSpeed += 1;
    }
     public static void main(String[] args) {
-        Car Honda = new Car(); // created an instance of the class car with object Honda
-        System.out.println("Honda car brand new Condition ");
-        Honda.printVariables(); 
-        
-        Car BMW = new Car(500,2000,'A');
-        BMW.upgradespeed();       
-        System.out.println("BMW updated values");
-        BMW.printVariables();
-        BMW.getIN();
-        BMW.getIN();
-        System.out.println("passengers :"+BMW.passengers);
-        BMW.getOut();
-        System.out.println("max fuel is :"+BMW.maxmilage());
-        
+       Car myCar = new Car();
+       System.out.println("Initial no of Passengers : " + myCar.noOfPassengers );
+       myCar.getIN();
+       myCar.getIN();
+       myCar.getIN();
+       myCar.getIN();
+       myCar.getIN();
+       myCar.getIN();
+       myCar.getOut();
+       myCar.getOut();
+       myCar.getOut();
+       myCar.getOut();
+       myCar.getOut();
         
             }
     

39995e260a658cb70403baad40f14a95e3d790c4
Premdeep Amudala <preamdhep.usapp@gmail.com>
2016-01-05 20:25:27 -0800

    Added Comments

--- GuessTheNumber/src/guessthenumber/GuessTheNumber.java
+++ GuessTheNumber/src/guessthenumber/GuessTheNumber.java
@@ -1,4 +1,3 @@
-
 package guessthenumber;
 
 import java.util.Random;
@@ -10,47 +9,46 @@ import java.util.Scanner;
  */
 public class GuessTheNumber {
 
-   int number;
-   int max = 100;
-   int secretNumber;
-   
-   public void randomNumber(){
-       
-       Random rand = new Random();
-       //int x = rand.nextInt();
-       this.secretNumber = Math.abs(rand.nextInt()) % (this.max+1);
-       
-   }
-   
-   public void playGame(){
-     Scanner sc = new Scanner(System.in);
-       
-        while(true){
+    int number;
+    int max = 100;
+    int secretNumber;
+
+    public void randomNumber() {
+
+        Random rand = new Random();
+        //int x = rand.nextInt();
+        // below statement keeps our secret number in between 0 and 100
+        // anyNumber % N = {0,1,2--- N-1}
+        this.secretNumber = Math.abs(rand.nextInt()) % (this.max + 1);
+
+    }
+
+    public void playGame() {
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
             this.number = sc.nextInt();
-            if(this.number > this.secretNumber ){
-                 System.out.println("Number is too High");
-            }
-               
-            else if (this.number < this.secretNumber){
-                 System.out.println("Number is too Small");
-            }
-               
-            else{
-                 System.out.println("Hurray you got the correct Number");
+            if (this.number > this.secretNumber) {
+                System.out.println("Number is too High");
+            } else if (this.number < this.secretNumber) {
+                System.out.println("Number is too Small");
+            } else {
+                System.out.println("Hurray you got the correct Number");
                 break;
             }
-               
+
         }
-        
-   }
-   
+
+    }
+
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hiii welocme to Guess the number game. Dare to win top prizes by guessing our secret number ");
-        System.out.println("LEts start the Game, Please guess the number");
+        System.out.println("Hiii welocme to Guess the number game. our secret number will be in between 0 and 100");
+        System.out.println("LEts start the Game,"
+                + " Please guess the number");
         GuessTheNumber guess = new GuessTheNumber();
         guess.randomNumber();
         guess.playGame();
-        
+
     }
 }

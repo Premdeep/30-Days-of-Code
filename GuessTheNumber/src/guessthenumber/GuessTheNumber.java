3af6051e9ed6da0b7aad4965d64fd764760d9efd
Premdeep Amudala <preamdhep.usapp@gmail.com>
2016-01-11 14:52:29 -0800

    Added a constructor with custom values

--- GuessTheNumber/src/guessthenumber/GuessTheNumber.java
+++ GuessTheNumber/src/guessthenumber/GuessTheNumber.java
@@ -8,30 +8,32 @@ import java.util.Scanner;
  * @author Prem
  */
 public class GuessTheNumber {
-
-    int number;
-    int max = 100;
+   
+    int max;
     int secretNumber;
-
-    public void randomNumber() {
-
+    
+    
+    // The constructor should be as same as the class name with out any return type before it
+    
+    public GuessTheNumber(){
         Random rand = new Random();
-        //int x = rand.nextInt();
-        // below statement keeps our secret number in between 0 and 100
-        // anyNumber % N = {0,1,2--- N-1}
-        this.secretNumber = Math.abs(rand.nextInt()) % (this.max + 1);
-
+        max = 100;
+       secretNumber = Math.abs(rand.nextInt()) % (max + 1);
+      
     }
+  
 
     public void playGame() {
-        Scanner sc = new Scanner(System.in);
+        
+         System.out.println("The secret number is : "+secretNumber);
 
         while (true) {
-            this.number = sc.nextInt();
-            if (this.number > this.secretNumber) {
-                System.out.println("Number is too High");
-            } else if (this.number < this.secretNumber) {
-                System.out.println("Number is too Small");
+            Scanner sc = new Scanner(System.in);
+            int number = sc.nextInt();
+            if (number > secretNumber) {
+                System.out.println("Your Number is too High");
+            } else if (number < secretNumber) {
+                System.out.println("Your Number is too Small");
             } else {
                 System.out.println("Hurray you got the correct Number");
                 break;
@@ -46,8 +48,7 @@ public class GuessTheNumber {
         System.out.println("Hiii welocme to Guess the number game. our secret number will be in between 0 and 100");
         System.out.println("LEts start the Game,"
                 + " Please guess the number");
-        GuessTheNumber guess = new GuessTheNumber();
-        guess.randomNumber();
+        GuessTheNumber guess = new GuessTheNumber();      
         guess.playGame();
 
     }
